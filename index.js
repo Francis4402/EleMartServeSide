e8b6769cb61607c164dd35bee280fd8d392caeea
Francis4402 <francism4402@gmail.com>
2024-01-26 12:47:30 +0600

    server updated

--- index.js
+++ index.js
@@ -39,6 +39,7 @@ const logger = async (req, res, next) => {
 const productCollection = client.db('elemart').collection('product');
 const userCollection = client.db('elemart').collection('users');
 const cartCollections = client.db('elemart').collection('cart');
+const featuredCollections = client.db('elemart').collection('featuredCollection');
 
 app.get('/', (req, res) => {
     res.send('server started')
@@ -87,6 +88,125 @@ app.get('/posts/:id', async (req, res) => {
 app.post('/addproduct', async (req, res) => {
     const post = req.body;
     const result = await productCollection.insertOne(post);
+    res.send(result);
+})
+
+app.get('/addproduct/:id', async (req, res) => {
+    const id = req.params.id;
+    const details = {_id: new ObjectId(id)}
+    const result = await productCollection.findOne(details);
+    res.send(result);
+})
+
+app.put('/addproduct/:id', async (req, res) => {
+    const id = req.params.id;
+    const filter = {_id: new ObjectId(id)}
+    const options = {upsert: true};
+    const updateProducts = req.body;
+    const products = {
+        $set: {
+            name: updateProducts.name,
+            image: updateProducts.image,
+            price: updateProducts.price,
+            modelname: updateProducts.modelname,
+            size: updateProducts.size,
+            displaytype: updateProducts.displaytype,
+            resolution: updateProducts.resolution,
+            brightness: updateProducts.brightness,
+            displayfeatures: updateProducts.displayfeatures,
+            chipset: updateProducts.chipset,
+            cputype: updateProducts.cputype,
+            gpu: updateProducts.gpu,
+            ram: updateProducts.ram,
+            internalstorage: updateProducts.internalstorage,
+            cardslot: updateProducts.cardslot,
+            rcamresolution: updateProducts.rcamresolution,
+            rcamfeatures: updateProducts.rcamfeatures,
+            rvideorecording: updateProducts.rvideorecording,
+            fcamresolution: updateProducts.fcamresolution,
+            fcamfeatures: updateProducts.fcamfeatures,
+            fcamvideorecording: updateProducts.fcamvideorecording,
+            speaker: updateProducts.speaker,
+            audiofeatures: updateProducts.audiofeatures,
+            sim: updateProducts.sim,
+            wifi: updateProducts.wifi,
+            bluetooth: updateProducts.bluetooth,
+            gps: updateProducts.gps,
+            nfc: updateProducts.nfc,
+            audiojack: updateProducts.audiojack,
+            operatingsystem: updateProducts.operatingsystem,
+            sensor: updateProducts.sensor,
+            iprating: updateProducts.iprating,
+            otherfeatures: updateProducts.otherfeatures,
+            batterytype: updateProducts.batterytype,
+            charging: updateProducts.charging,
+            dimension: updateProducts.dimension,
+            weight: updateProducts.weight,
+            materials: updateProducts.materials,
+            colors: updateProducts.colors,
+            warranty: updateProducts.warranty,
+            installationspolicy: updateProducts.installationspolicy,
+            actype: updateProducts.actype,
+            technology: updateProducts.technology,
+            capacity: updateProducts.capacity,
+            coverage: updateProducts.coverage,
+            energysavingrating: updateProducts.energysavingrating,
+            capacityofcooling: updateProducts.capacityofcooling,
+            noiselevel: updateProducts.noiselevel,
+            compressortype: updateProducts.compressortype,
+            condenser: updateProducts.condenser,
+            refrigeranttype: updateProducts.refrigeranttype,
+            others: updateProducts.others,
+            powertype: updateProducts.powertype,
+            powerconsumption: updateProducts.powerconsumption,
+            acweight: updateProducts.acweight,
+            drivermagnet: updateProducts.drivermagnet,
+            impedance: updateProducts.impedance,
+            headphonesensitivity: updateProducts.headphonesensitivity,
+            inputjack: updateProducts.inputjack,
+            driverdiameter: updateProducts.driverdiameter,
+            connectivity: updateProducts.connectivity,
+            headphonesdimensions: updateProducts.headphonesdimensions,
+            headphonesweight: updateProducts.headphonesweight,
+            headphonescolor: updateProducts.headphonescolor,
+            headphonescablelength: updateProducts.headphonescablelength,
+            microphoneSize: updateProducts.microphoneSize,
+            microphonesensitivity: updateProducts.microphonesensitivity,
+            microphonepickup: updateProducts.microphonepickup,
+            droneweight: updateProducts.droneweight,
+            dronemaxspeed: updateProducts.dronemaxspeed,
+            dronemaxflighttime: updateProducts.dronemaxflighttime,
+            dronefov: updateProducts.dronefov,
+            feature1: updateProducts.feature1,
+            feature2: updateProducts.feature2,
+            feature3: updateProducts.feature3,
+            feature4: updateProducts.feature4,
+            description1title: updateProducts.description1title,
+            description2title: updateProducts.description2title,
+            description3title: updateProducts.description3title,
+            description4title: updateProducts.description4title,
+            description5title: updateProducts.description5title,
+            description6title: updateProducts.description6title,
+            description7title: updateProducts.description7title,
+            description1: updateProducts.description1,
+            description2: updateProducts.description2,
+            description3: updateProducts.description3,
+            description4: updateProducts.description4,
+            description5: updateProducts.description5,
+            description6: updateProducts.description6,
+            description7: updateProducts.description7,
+            categories: updateProducts.categories,
+            brands: updateProducts.brands,
+        }
+    }
+    const result = await productCollection.updateOne(filter, products, options);
+    res.send(result);
+})
+
+app.delete('/addproduct/:id', async (req, res) => {
+    const id = req.params.id;
+    const query = {_id: new ObjectId(id)};
+    const result = await productCollection.deleteOne(query);
     res.send(result);
 })
 
